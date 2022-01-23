0f5fc7653907bfee4cc6e479efa4f0e8bca61e50
patrick-katambi <katambipatrick@gmail.com>
2022-01-23 15:54:30 +0000

    connection testing

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ function getConfig() {
     const config = {
         MODE: process.env.NODE_ENV || 'development',
         PORT: process.env.SERVER_PORT || 3000,
-        MONGO_CONNECTION: process.env.MONGO_CONNECTION,
+        MONGO_CONNECTION: process.env.MONGO_CONNECTION || "mongodb+srv://patrick-katambi:perpheloconupus@cluster0.xzcp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     }
 
     return config
