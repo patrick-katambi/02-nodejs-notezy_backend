970c3f2ba7e1130e3fd664ad583acaaed9a51f19
patrick-katambi <katambipatrick@gmail.com>
2022-01-23 15:58:30 +0000

    port name

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 function getConfig() {
     const config = {
         MODE: process.env.NODE_ENV || 'development',
-        PORT: process.env.SERVER_PORT || 3000,
+        PORT: process.env.PORT || 3000,
         MONGO_CONNECTION: process.env.MONGO_CONNECTION || "mongodb+srv://patrick-katambi:perpheloconupus@cluster0.xzcp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     }
 
